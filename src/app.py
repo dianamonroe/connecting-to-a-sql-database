779fa786742f02cb38132f270d47edc1f697d68a
dianamonroe <dianamonroe79@gmail.com>
2024-10-31 13:02:04 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -47,13 +47,14 @@ CREATE TABLE book_authors(
     author_id INT NOT NULL,
     PRIMARY KEY(book_id, author_id),
     CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
-    CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE)
+    CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
 );
 """)
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 
-db_engine.execute("""INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
+db_engine.execute("""
+INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
 INSERT INTO publishers(publisher_id, name) VALUES (4, 'Academic Press');
@@ -71,7 +72,7 @@ INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (7, '
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (8, 'Paul', null, 'Albitz');
 
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (1, 'Lean Software Development: An Agile Toolkit', 240, 4.17, '9780320000000', '2003-05-18', 5);
-INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (2, 'Facing the Intelligence Explosion', 91, 3.87, null, '2013-02-01', 7);"
+INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (2, 'Facing the Intelligence Explosion', 91, 3.87, null, '2013-02-01', 7);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (3, 'Scala in Action', 419, 3.74, '9781940000000', '2013-04-10', 1);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (4, 'Patterns of Software: Tales from the Software Community', 256, 3.84, '9780200000000', '1996-08-15', 1);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (5, 'Anatomy Of LISP', 446, 4.43, '9780070000000', '1978-01-01', 3);
@@ -99,9 +100,5 @@ INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);
 df_books = pd.read_sql_query("SELECT * FROM books", db_engine)
 print(store_db_engine_authors)
 
-#Opción B)
-df_authors_2 = db_engine.execute("SELECT * FROM book_authors") #SEE HOW THIS WORKS BELOW
-for row in df_engine_authors: # Iterate through all the rows of the resulting filter
-    print(f"book_id = {row[0]}  author_id = {row[1]}")
 
     
